39193bcc8580dfcab5cbca9be77fd5492cd4d099
Andrej Koller <andrejkoller@outlook.com>
2026-01-05 13:41:26 +0100

    refactor: remove Russian localization and add German localization; update i18n configuration

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,11 @@
 import { createI18n } from "vue-i18n";
-import ru from "./locales/ru.json";
 import en from "./locales/en.json";
+import de from "./locales/de.json";
 
 const i18n = createI18n({
   locale: navigator.language,
   fallbackLocale: "en",
-  messages: { ru, en },
+  messages: { en, de },
   legacy: false,
 });
 
